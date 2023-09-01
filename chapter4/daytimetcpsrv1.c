db0a208191e11a436db58e339278081ed06bee25
grinfield <grinfield.me@gmail.com>
2023-09-01 11:08:58 +0800

    feat: use getsockname to obtain local address and port of connected socket

--- chapter4/daytimetcpsrv1.c
+++ chapter4/daytimetcpsrv1.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     socklen_t len;
     char buff[BUFSIZ];
     time_t t;
-    struct sockaddr_in servaddr, cliaddr;
+    struct sockaddr_in servaddr, cliaddr, tmpaddr;
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     if (listenfd < 0){
@@ -51,12 +51,21 @@ int main(int argc, char *argv[])
             perror("accept()");
             exit(1);
         }
-
         printf("Connection from: %s, port: %d\n", 
                 inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff)),
                 ntohs(cliaddr.sin_port));
-        t = time(NULL);
 
+        len = sizeof(tmpaddr);
+        ret = getsockname(connfd, (struct sockaddr *)&tmpaddr, &len);
+        if (ret < 0){
+            perror("getsockname()");
+            exit(1);
+        }
+        printf("Server address: %s, port: %d\n", 
+                inet_ntop(AF_INET, &tmpaddr.sin_addr, buff, sizeof(buff)),
+                ntohs(tmpaddr.sin_port));
+
+        t = time(NULL);
         snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&t));
         ret = write(connfd, buff, strlen(buff));
         if (ret < 0){
@@ -64,6 +73,7 @@ int main(int argc, char *argv[])
             exit(1);
         }
         shutdown(connfd, SHUT_RDWR);
+        close(connfd);
     }
 
     exit(0);
